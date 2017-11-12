ccbd018aedff5cd660f4af7c207eca607ecea450
bethtechow <bethtechow@yahoo.com>
2017-11-12 20:42:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ function BoardMember(name, homeState, training){
   this.name=name;
   this.homeState=homeState;
   this.training=training;
-  this.veto = function(){
+  BoardMember.prototype.veto = function(){
     return("No, I must disagree");
-  this.approve = function(){
+  BoardMember.prototype.approve = function(){
       return("You can do that!");
-  this.doCharity= function(){
+  BoardMember.prototype.doCharity= function(){
       return("I like to help people.");
-    this.veto = function(){
-          return("No, I must disagree");
+  BoardMember.prototype.releasePressStatement= function(){
+      return("You will see great things from Scuber.");
+  BoardMember.prototype.sayHI = funcition(){
+    
+  }
   }
 
 }
